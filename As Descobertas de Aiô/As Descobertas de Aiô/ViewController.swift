daa898471379ebc7d00e7e44b7ef6648efa46bda
Bassani-Paulo <32801209+Bassani-Paulo@users.noreply.github.com>
2021-06-29 09:20:43 -0300

    Creio o projeto

---
+++ As Descobertas de Aiô/As Descobertas de Aiô/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  As Descobertas de Aiô
+//
+//  Created by Paulo Bassani on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
