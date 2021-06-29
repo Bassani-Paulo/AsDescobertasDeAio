b9504f91b20bedacd85811afbeea88e35b566904
Francisca Rosa <f.rosa96@hotmail.com>
2021-06-29 12:09:36 -0300

    Base do projeto: pastas separadas e uma referência de teste criada

---
+++ As Descobertas de Aiô/As Descobertas de Aiô/Ajustes/AjustesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AjustesViewController.swift
+//  As Descobertas de Aiô
+//
+//  Created by Francisca Rosa on 29/06/21.
+//
+
+import UIKit
+
+class AjustesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
