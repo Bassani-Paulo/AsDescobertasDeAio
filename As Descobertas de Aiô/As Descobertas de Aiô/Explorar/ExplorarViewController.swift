b177c8de3e246b4bb7fb2fd69f19f9f62db4fae9
PedroHaruke <pedrorinzo@gmail.com>
2021-07-12 09:45:01 -0300

    explorarScreen

--- As Descobertas de Aiô/As Descobertas de Aiô/Explorar/ExplorarViewController.swift
+++ As Descobertas de Aiô/As Descobertas de Aiô/Explorar/ExplorarViewController.swift
@@ -5,25 +5,159 @@
 //  Created by Francisca Rosa on 29/06/21.
 //
 
+import youtube_ios_player_helper
 import UIKit
 
+struct CustomData {
+    var title: String
+    var videoID: String
+}
+
 class ExplorarViewController: UIViewController {
+    
+    let data = [
+        CustomData(title: "COMO FAZER PAPEL RECICLADO em casa ",
+                   videoID: "fjt5gWCx120"),
+        CustomData(title: "Como reciclar tubo de pasta de dente em casa ",
+                   videoID: "vAnuoulkzdI"),
+        CustomData(title: "Como fazer um pulmão artificial caseiro ",
+                   videoID: "DNbF6bnCoio"),
+    ]
+
+    fileprivate let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 50
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Fundo.png")!)
+        
+        view.addSubview(collectionView)
+        view.addSubview(exploreLabel)
+        view.addSubview(closeExploreButton)
+        view.addSubview(lineView)
+        
+        collectionView.backgroundColor = UIColor(white: 1, alpha: 0)
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 97).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        exploreLabel.widthAnchor.constraint(equalToConstant: 75).isActive = true
+        exploreLabel.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        exploreLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 26).isActive = true
+        exploreLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -335).isActive = true
+        exploreLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 63).isActive = true
+        
+        closeExploreButton.widthAnchor.constraint(equalToConstant: 23).isActive = true
+        closeExploreButton.heightAnchor.constraint(equalToConstant: 23).isActive = true
+        closeExploreButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 26).isActive = true
+        closeExploreButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -335).isActive = true
+        closeExploreButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30).isActive = true
+        
+        
+        lineView.widthAnchor.constraint(equalToConstant: 700).isActive = true
+        lineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        lineView.topAnchor.constraint(equalTo: view.topAnchor, constant: 63).isActive = true
+        lineView.leftAnchor.constraint(equalTo: view.leftAnchor , constant: 63).isActive = true
+        lineView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -63).isActive = true
 
-        // Do any additional setup after loading the view.
     }
     
+    lazy var lineView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    lazy var exploreLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Explorar"
+        label.font = UIFont(name: "SF Pro Rounded", size: CGFloat(30))
+        return label
+    }()
+    
+    lazy var closeExploreButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
+        button.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(self.backAction(_:)), for: .touchUpInside)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        return button
+    }()
+    
+    @IBAction func backAction(_ sender: UIButton!) {
+       let _ = self.dismiss(animated: true, completion: nil)
+        print("tapped button")
     }
-    */
 
 }
+
+extension ExplorarViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2, height: collectionView.frame.width/3.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.data = self.data[indexPath.row]
+        return cell
+    }
+    
+}
+
+class CustomCell: UICollectionViewCell {
+    
+    var data: CustomData? {
+        didSet {
+            guard let data = data else { return }
+            yt.load(withVideoId: data.videoID,
+                    playerVars: ["playsinline":1])
+        }
+    }
+    
+    fileprivate let yt: YTPlayerView = {
+        let youtubeVideoView = YTPlayerView()
+        youtubeVideoView.load(withVideoId: "aT6xMMGQLBc")
+        youtubeVideoView.translatesAutoresizingMaskIntoConstraints = false
+        youtubeVideoView.contentMode = .scaleAspectFill
+        youtubeVideoView.clipsToBounds = true
+        youtubeVideoView.layer.cornerRadius = 10
+        return youtubeVideoView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(yt)
+        yt.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        yt.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        yt.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        yt.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
---
+++ As Descobertas de Aiô/Pods/Target Support Files/Pods-As Descobertas de Aiô/Pods-As Descobertas de Aiô-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_As_Descobertas_de_Aio_ : NSObject
+@end
+@implementation PodsDummy_Pods_As_Descobertas_de_Aio_
+@end
